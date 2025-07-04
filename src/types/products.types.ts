6af159a7585f749ce553cc126981a68c759d6cb5
monsterluk <lukasz.sikorra@gmail.com>
2025-07-04 09:36:38 +0200

    Update products.types.ts

--- src/types/products.types.ts
+++ src/types/products.types.ts
@@ -1,20 +1,2 @@
 // Product types for plexi_appka
 
-export type ProductType = 'container' | 'cabinet' | 'enclosure' | 'wall';
-
-export interface Product {
-  id: string;
-  name: string;
-  type: ProductType;
-  description: string;
-}
-
-export interface ProductConfig {
-  wallThickness: number;
-  edgeThickness: number;
-  minDimensions: {
-    width: number;
-    height: number;
-    depth: number;
-  };
-}
