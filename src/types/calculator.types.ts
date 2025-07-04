7233a996e2038873ab9557e2f6290935df721dd1
monsterluk <lukasz.sikorra@gmail.com>
2025-07-04 09:35:50 +0200

    Update calculator.types.ts

--- src/types/calculator.types.ts
+++ src/types/calculator.types.ts
@@ -1,17 +1,2 @@
 // Calculator types for plexi_appka
 
-export interface CalculatorState {
-  dimensions: {
-    width: number;
-    height: number;
-    depth: number;
-  };
-  material: string;
-  product: string;
-}
-
-export interface CalculationResult {
-  surface: number;
-  weight: number;
-  price: number;
-}
